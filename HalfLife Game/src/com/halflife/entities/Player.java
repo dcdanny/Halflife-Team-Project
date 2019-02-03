83f35f16d63ee2d02caa32dcabc202f1e32ebd13
David <DGR790@student.bham.ac.uk>
2019-02-03 15:06:49 +0000

    Test Commit

--- HalfLife Game/src/com/halflife/entities/Player.java
+++ HalfLife Game/src/com/halflife/entities/Player.java
@@ -8,15 +8,24 @@ public class Player extends KeyAdapter {
 	
 	
 	
+	// This will detect when a keyboard button is pressed, which controls the player.
 	public void keyPressed(KeyCode key) {
 		
 		if (key == KeyCode.W) {
-			//DO SOMETHING
 		 	System.out.println("Pressed W");
+		} 
+		else if (key == KeyCode.S) {
+			System.out.println("Pressed S");
+		} 
+		else if (key == KeyCode.A) {
+			System.out.println("Pressed A");
 		}
-			
+		else if (key == KeyCode.D) {
+			System.out.println("Pressed D");
+		}	
 	}
 	
+	// This will detect when a keyboard button has been released.
 	public void keyReleased(KeyCode key) {
 		if (key == KeyCode.W) {
 			//STOP DOING SOMETHING
