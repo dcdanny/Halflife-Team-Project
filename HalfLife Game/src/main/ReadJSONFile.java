8559285443006335beadbd3f31156bce9026eaac
Dan <dcdannyclarke@gmail.com>
2019-02-18 12:56:15 +0000

    Started JSON File Reader (added Jar Dependency)

---
+++ HalfLife Game/src/main/ReadJSONFile.java
@@ -0,0 +1,14 @@
+package main;
+
+
+import java.io.*;
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ReadJSONFile {
+	JSONParser parser = new JSONParser();
+}
