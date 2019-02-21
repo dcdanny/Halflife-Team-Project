3604cd3ed6aca51b47ed9e5d3c84364294978333
David <DGR790@student.bham.ac.uk>
2019-02-21 14:03:07 +0000

    Small change to Game

--- HalfLife Game/src/main/Game.java
+++ HalfLife Game/src/main/Game.java
@@ -57,7 +57,6 @@ public class Game extends Application {
 		//root.getChildren().add(spike1);
 		//root.setStyle("-fx-background-color: #4f7b8a;");
 		
-		
 		//root.getChildren().add(DeathShow);
 		
 		//display.getChildren().addAll(root);
@@ -152,21 +151,9 @@ public class Game extends Application {
 	private void tick() {
 		player.tick();
 		checkCollision(player);
-		checkPos(player);
 
 	}
 	
-	private void checkPos(Player player) {
-		// TODO Auto-generated method stub
-		double x =player.getXLocation();
-	
-		if (x>400) {
-			root.setLayoutX(root.getTranslateX()-(x-400));
-		}
-		
-		
-	}
-
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage.setResizable(false);
@@ -188,28 +175,15 @@ public class Game extends Application {
 	private void buttonPressing(Scene s) {
 	
 		s.setOnKeyPressed(e-> {
-			double x=player.getXLocation();
 			switch (e.getCode()) {
 			case A:
-			//	if (x<405) {
-				//	root.setLayoutX(root.getTranslateX()+(405-x));
-			//	}
-				
-				//System.out.println("layout"+root.getLayoutX());
 				player.setVelX(-5);
-				
+				root.setLayoutX(root.getLayoutX()+10);
 				break;
 			case D: 
-				
-				
-			//	//if (x>root.getMaxWidth())
-			//	if (x>405) {
-			//		root.setLayoutX(root.getTranslateX()-(x-405));
-			//	}
-			
 				player.setVelX(5);
-				
-				//root.setStyle("-fx-background-color: #4f7b8a;");
+				root.setLayoutX(root.getLayoutX()-10);
+				root.setStyle("-fx-background-color: #4f7b8a;");
 				break;
 			case S: 
 				player.setVelY(5);
@@ -217,7 +191,6 @@ public class Game extends Application {
 			case W:
 				if (player.getGravity() == 0) {
 				player.jump();
-				
 				}
 				break;
 			case SPACE:
