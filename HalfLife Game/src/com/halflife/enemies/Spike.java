9e25fe6b1655b456edfcbe964e9c05dbb5722e37
ben772 <ben772@student.bham.ac.uk>
2019-02-22 00:56:07 +0000

    gave spike a string type name for collisions

--- HalfLife Game/src/com/halflife/enemies/Spike.java
+++ HalfLife Game/src/com/halflife/enemies/Spike.java
@@ -4,12 +4,19 @@ import javafx.scene.shape.Polygon;
 
 public class Spike extends Polygon{
 	
+	private final String type;
+	
 	public Spike(double[] points) {
 		super(points);
+		type = "spike";
 	}
 	
 	void PlayerCollision() {
 		
+	}
+	
+	public String getType(){
+		return type;
 	}
 
 }
