b886ac605a46fdb2661824bc232b79e8098732c6
Dan <dcdannyclarke@gmail.com>
2019-03-03 16:36:21 +0000

    Can choose level to start in game

--- HalfLife Game/src/menu/view/LevelMenuController.java
+++ HalfLife Game/src/menu/view/LevelMenuController.java
@@ -21,8 +21,8 @@ public class LevelMenuController {
 	
 	// The "1" Button, directing to the Game "Level 1"
 	@FXML
-	private void go1() throws IOException {
-		new Thread() {
+	private void go1() throws Exception {
+		/*new Thread() {
 			@Override
 			public void run() {
 				Platform.runLater(() -> {
@@ -32,28 +32,35 @@ public class LevelMenuController {
 						e.printStackTrace();
 					}
 				});
-				}
-			}.start();	
+				}*/
+			Game game = new Game();
+			game.setCurrentLevel(Level_Info.LEVEL1);
+			game.start(primaryStage);
+			//}.start();	
 	}
 	
 	// The "2" Button, directing to the Game "Level 2"
 	@FXML
-	private void go2() throws IOException {
-	/*	Game game = new Game();
-		game.setLevel(Level_Info.LEVEL2);
-		game.start(primaryStage);*/
+	private void go2() throws Exception {
+		Game game = new Game();
+		game.setCurrentLevel(Level_Info.LEVEL2);
+		game.start(primaryStage);
 	}
 	
 	// The "3" Button, directing to the Game "Level 3"
 	@FXML
-	private void go3() throws IOException {
-		
+	private void go3() throws Exception {
+		Game game = new Game();
+		game.setCurrentLevel(Level_Info.LEVEL3);
+		game.start(primaryStage);
 	}
 	
 	// The "4" Button, directing to the Game "Level 4"
 	@FXML
-	private void go4() throws IOException {
-		
+	private void go4() throws Exception {
+		Game game = new Game();
+		game.setCurrentLevel(Level_Info.LEVEL4);
+		game.start(primaryStage);
 	}
 	
 	// The "BACK" Button, directing to the main menu "HALFLIFE"
