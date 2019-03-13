940f691d607ddf82be8f05fe7450386076aa210e
Dan <dcdannyclarke@gmail.com>
2019-03-13 18:51:19 +0000

    Task threading to start game when connection received
    Slightly messy code

--- HalfLife Game/src/menu/view/WaitScreenController.java
+++ HalfLife Game/src/menu/view/WaitScreenController.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
-
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -23,25 +25,39 @@ public class WaitScreenController {
 	private Stage primaryStage;
 	@FXML private TextField ipAddrInput;
 	private FutureTask msga1;
+	private Client client;
+	String msgR;
 	
 	public void setStage(Stage stage, Client client){
+		this.client = client;
 		primaryStage = stage;
 		//initialise(stage, client);
-		CallableExample callableexample = new CallableExample();
+		//CallableExample callableexample = new CallableExample();
 		try {
-			callableexample.setClient(client);
+			//callableexample.setClient(client);
 			//callableexample.call();
 			
 		      // Create the FutureTask with Callable 
-		      msga1 = new FutureTask(callableexample); 
-		  
+		      //msga1 = new FutureTask(callableexample); 
+			
+			
 		      // As it implements Runnable, create Thread 
-		      // with FutureTask 
-		      Thread t = new Thread(msga1); 
+		      // with FutureTask
+			
+			 Task<Integer> task = new Task<Integer>() {
+		         @Override protected Integer call() throws Exception {
+		        	 msgR = client.waitForMessage().getText();
+		             System.out.println("msgRCeiVeDD " + msgR);
+		             return 65;
+		         }
+		     };
+			
+		      Thread t = new Thread(task); 
 		      t.start();
 		      
-			
-			System.out.println("m");
+		      task.setOnSucceeded(event -> startGame(msgR, task.getValue()));
+
+		     System.out.println("m");
 
 			//System.out.println("Get: "+ msga1.get());
 			System.out.println("n");
@@ -59,8 +75,28 @@ public class WaitScreenController {
 	public void waitConnection() {	
 		System.out.println("STARTGAME");
 	}
+	
+	/*Object result;
+	Task<Integer> task2 = new Task<Integer>() {;
+	task2.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+	    @Override
+	    public void handle(WorkerStateEvent t) {
+	        result = task2.getValue();
+	    }
+	});
+	}*/
 
-	class CallableExample implements Callable { 
+
+	
+	/*Task<Integer> task = new Task<Integer>() {
+	    @Override protected Integer call() throws Exception {
+	    	String msg = null;
+	    	msg = client.waitForMessage().getText();
+
+	        return iterations;
+	    }
+	};*/
+	/*class CallableExample implements Callable { 
 	  
 		private Client client;
 		
@@ -76,7 +112,8 @@ public class WaitScreenController {
 //	    	Thread.sleep(2 * 1000);
 	    	System.out.println("ha");
 	    	msg = client.waitForMessage().getText();
-	    	
+            /*Platform.runLater(() -> {
+            
 			switch (msg){
 				case "lvl1":
 					System.out.println("here 1234567890");
@@ -92,7 +129,12 @@ public class WaitScreenController {
 					
 					Game game = new Game();
 					game.setCurrentLevel(Level_Info.LEVEL1);
+				try {
 					game.start(primaryStage);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 					primaryStage.show();
 					break;
 				case "lvl2":
@@ -105,13 +147,15 @@ public class WaitScreenController {
 					
 					break;					
 			}
+            });*//*
+	    	
 	        // To simulate a heavy computation, 
 	        // we delay the thread for some random time 
 //	        Thread.sleep(2 * 1000); 
 	  
 	        return 45; 
 	    } 
-	}
+	}*/
 	
 	// The "Disconnect" Button
 	@FXML
@@ -125,6 +169,42 @@ public class WaitScreenController {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-
+	private void startGame(String msg,int ret){
+		System.out.println("message:"+msg+" Return:"+ret);
+		
+		switch (msg){
+			case "lvl1":
+				System.out.println("here 1234567890");
+	//			FXMLLoader loader = new FXMLLoader(getClass().getResource("mainmenu.fxml"));
+	//			Pane levelMenu = loader.load();
+	//			MainMenuController controller = loader.getController();
+	//			controller.setStage(primaryStage);
+	//			Scene scene = new Scene(levelMenu);
+	//			primaryStage.setScene(scene);
+	//			primaryStage.show();
+				
+				
+				
+				Game game = new Game();
+				game.setCurrentLevel(Level_Info.LEVEL1);
+			try {
+				game.start(primaryStage);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				primaryStage.show();
+				break;
+			case "lvl2":
+				
+				break;
+			case "lvl3":
+				
+				break;
+			case "lvl4":
+				
+				break;					
+		}
+	}
 
 }
