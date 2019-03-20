95625d74a36d7103d87e242f14648dfb8c12df0f
David <dgr790@student.bham.ac.uk>
2019-03-20 14:53:31 +0000

    Javadoc for rectObject class

--- HalfLife Game/src/com/halflife/entities/RectObject.java
+++ HalfLife Game/src/com/halflife/entities/RectObject.java
@@ -15,20 +15,32 @@ import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import javafx.scene.layout.Pane;
 
+/**
+ * RectObject --- Creates all objects in the game that the
+ * player can collide with
+ * @author Halflife
+ */
 public class RectObject extends Rectangle{
+	
 	protected FadeTransition ft;
     protected Rectangle rect;
-
     public float gravity = 0.5f;
 	private boolean falling = true;
 	private boolean jumping = false;
-    
 	protected boolean dead= false;
 	private final String type;
 	private double x,y,w,h;
 	private Color col;
 
-	
+	/**
+	 * Constructor of each rectangular object
+	 * @param x X coordinate of the object
+	 * @param y Y coordinate of the object
+	 * @param width Width of the object
+	 * @param height Height of the object
+	 * @param type Type of the object
+	 * @param col Colour of the object
+	 */
 	public RectObject(double x, double y,int width, int height, String type, Color col){
 		super (width,height,col);
 		this.type =type;
@@ -38,33 +50,35 @@ public class RectObject extends Rectangle{
 		this.h=height;
 		this.col=col;
 
-		
-		//playerFade();
-
 	 	setTranslateX(x);
 		setTranslateY(y);
-
-		
-
-		/* Ignore for now, i was testing using images on top of shapes
-		 Image player = null;
-		try {
-			player = new Image(new FileInputStream("res/ghostchar.png"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 this.setFill(new ImagePattern(player, 0, 0, 50, 50, false));
-		*/
 	}
+	
+	/**
+	 * Movement of the object in the X direction
+	 * @param i Amount to move the object by
+	 */
 	public void moveX(int i) {
-		setTranslateX(getTranslateX()+i); // you could change this, idk try implement speed? rather than straight translate
+		setTranslateX(getTranslateX()+i);
 	}
+	
+	/**
+	 * Movement of the object in the Y direction
+	 * @param i Amount to move the object by
+	 */
 	public void moveY(int i) {
 		setTranslateY(getTranslateY()+i);	
 	}
 	
 
+	/**
+	 * Creates the bullet object for when the player shoots
+	 * @param shooter Player object where the bullet comes from
+	 * @param colour Colour of the bullet
+	 * @param root Contains all objects that the bullet could interact with
+	 * @param bulletDir Determines which direction the bullet travels in
+	 * @return Bullet object
+	 */
 	public Bullet getBullet(RectObject shooter, Color colour, Pane root, boolean bulletDir) {
 		int moveBulletX = 0;
 		if (bulletDir == true) {
@@ -75,42 +89,85 @@ public class RectObject extends Rectangle{
 		Bullet bullet = new Bullet(shooter.getTranslateX() + moveBulletX, shooter.getTranslateY(), 20, 5, shooter.getType() + "bullet", Color.GREEN, root, bulletDir);
 		return bullet;
 	}
+	
+	/**
+	 * Returns the type of the current object
+	 * @return Type of the current object
+	 */
 	public String getType() {
 		return type;
 	}
+	
+	/**
+	 * Returns whether the current object is dead or not
+	 * @return Whether the current object is dead or not
+	 */
 	public boolean isDead() {
 		return dead;
 	}
+	
+	/**
+	 * Sets the state of deadness for the current object
+	 * @param dead Whether the current object is dead or not
+	 */
 	public void setDead(boolean dead) {
 		this.dead = dead;
 	}
+	
+	/**
+	 * Returns whether the current object is falling or not
+	 * @return Whether the current object is falling or not
+	 */
 	public boolean isFalling() {
 		return falling;
 	}
+	
+	/**
+	 * Sets the state of falling for the current object
+	 * @param falling Whether the current object is falling or not
+	 */
 	public void setFalling(boolean falling) {
 		this.falling = falling;
 	}
+	
+	/**
+	 * Returns whether the current object is jumping or not
+	 * @return Whether the current object is jumping or not
+	 */
 	public boolean isJumping() {
 		return jumping;
 	}
+	
+	/**
+	 * Sets the state of jumping for the current object
+	 * @param jumping Whether the current object is jumping or not
+	 */
 	public void setJumping(boolean jumping) {
 		this.jumping = jumping;
 	}
 
+	/**
+	 * Returns the Y location of the current object
+	 * @return Y location of the current object
+	 */
 	public double getYLocation() {
 		return this.getTranslateY();
 	}
+	
+	/**
+	 * Returns the X location of the current object
+	 * @return X location of the current object
+	 */
 	public double getXLocation() {
 		return this.getTranslateX();
 	}
 	
+	/**
+	 * Returns a string variation of the current object
+	 * @return String variation of the current object
+	 */
 	@Override
 	public String toString() {
 		return "RectObject x: "+x+ " y: "+ y +" w: "+ w +" h: " + h;
-		
 	}
-	
-
-	
-//moving up and down will need to apply physics so its not included but we could try move up down for moving platforms (later)
 }
