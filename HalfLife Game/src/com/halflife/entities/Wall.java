286fd4b8659411fdf700c7ba82f7eaa2b12ef66f
David <dgr790@student.bham.ac.uk>
2019-03-20 15:38:12 +0000

    Javadoc comments

--- HalfLife Game/src/com/halflife/entities/Wall.java
+++ HalfLife Game/src/com/halflife/entities/Wall.java
@@ -3,11 +3,22 @@ package com.halflife.entities;
 import javafx.scene.paint.Color;
 import main.GameConstants;
 
+/**
+ * Wall --- Creates the invisible wall objects in the game to
+ * stop the player from passing certain boundaries
+ * @author Halflife
+ */
 public class Wall extends RectObject{
 
-	public Wall(double d, double e, int width, int height) {
-		super(d, e, width, height, GameConstants.TYPE_WALL, Color.TRANSPARENT);
-		// TODO Auto-generated constructor stub
+	/**
+	 * Constructor for the wall object
+	 * @param x X coordinate of the wall object
+	 * @param e Y coordinate of the wall object
+	 * @param width Width of the wall object
+	 * @param height Height of the wall object
+	 */
+	public Wall(double x, double y, int width, int height) {
+		super(x, y, width, height, GameConstants.TYPE_WALL, Color.TRANSPARENT);
 	}
 
 }
