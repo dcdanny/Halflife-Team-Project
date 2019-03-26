95b42ef99b345578bd3e1c917776b526b2cdd512
txl658 <txl658@cs.bham.ac.uk>
2019-03-26 15:33:29 +0000

    fixing audio

--- HalfLife Game/src/menu/view/SettingsMenuController.java
+++ HalfLife Game/src/menu/view/SettingsMenuController.java
@@ -10,6 +10,7 @@ import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -32,52 +33,78 @@ public class SettingsMenuController {
 	private MediaPlayer mediaplayer;
 	//private Button soundOn, soundOff, musicOn, musicOff;
 	
-	public void start (Stage stage) {
+	//public void start (Stage stage) {
 		
-		Button soundOn = new Button("soundOn");
-		Button soundOff = new Button("soundOff");
-		Button musicOn = new Button("musicOn");
-		Button musicOff = new Button("musicOff");
+	//	Button soundOn = new Button("soundOn");
+	//	Button soundOff = new Button("soundOff");
+	//	Button musicOn = new Button("musicOn");
+	//	Button musicOff = new Button("musicOff");
 		
-		soundOn.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent arg0) {
-				Media soundFile = new Media("shootSound.mp3");
-				mediaplayer = new MediaPlayer(soundFile);
-				mediaplayer.stop();
-			}	
-		});
+		//soundOn.setOnMouseClicked((MouseEvent e) -> {
+				//Media soundFile = new Media("shootSound.mp3");
+				//mediaplayer = new MediaPlayer(soundFile);
+				//mediaplayer.play();
+				//System.out.println("sdasfasf");
+			//}	
+		//);
 		
-		soundOff.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				Media soundFile = new Media("shootSound.mp3");
-				mediaplayer = new MediaPlayer(soundFile);
-				mediaplayer.stop();
-			}	
-		});
+		//soundOff.setOnAction(new EventHandler<ActionEvent>() {
+		//@Override
+		//public void handle(ActionEvent arg0) {
+		//Media soundFile = new Media("shootSound.mp3");
+		//mediaplayer = new MediaPlayer(soundFile);
+		//mediaplayer.stop();
+		//}	
+		//});
 		
-		musicOn.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				Media musicFile = new Media("Sample - summer.mp3");
-				mediaplayer = new MediaPlayer(musicFile);
-				mediaplayer.play();
-			}	
-		});
+		//musicOn.setOnAction(new EventHandler<ActionEvent>() {
+		//@Override
+			//public void handle(ActionEvent arg0) {
+		//Media musicFile = new Media("Sample - summer.mp3");
+		//mediaplayer = new MediaPlayer(musicFile);
+		//mediaplayer.play();
+		//}	
+		//});
 		
-		musicOff.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				Media musicFile = new Media("Sample - summer.mp3");
-				mediaplayer = new MediaPlayer(musicFile);
-				mediaplayer.stop();
-			}	
-		});
+		//musicOff.setOnAction(new EventHandler<ActionEvent>() {
+		//@Override
+		//public void handle(ActionEvent arg0) {
+		//Media musicFile = new Media("Sample - summer.mp3");
+		//mediaplayer = new MediaPlayer(musicFile);
+		//mediaplayer.stop();
+		//}	
+		//});
 
-		mediaplayer.setAutoPlay(true);
-		mediaplayer.setVolume(0.1);	
+		//mediaplayer.setAutoPlay(true);
+		//mediaplayer.setVolume(0.1);	
+	//}
+	
+	@FXML
+	private void soundOn() throws IOException {
+		Media soundFile = new Media("shootSound.mp3");
+		mediaplayer = new MediaPlayer(soundFile);
+		mediaplayer.play();
+	}
+	
+	@FXML
+	private void soundOff() throws IOException {
+		Media soundFile = new Media("shootSound.mp3");
+		mediaplayer = new MediaPlayer(soundFile);
+		mediaplayer.stop();
+	}
+	
+	@FXML
+	private void musicOn() throws IOException {
+		Media musicFile = new Media("Sample - summer.mp3");
+		mediaplayer = new MediaPlayer(musicFile);
+		mediaplayer.play();
+	}
+	
+	@FXML
+	private void musicOff() throws IOException {
+		Media musicFile = new Media("Sample - summer.mp3");
+		mediaplayer = new MediaPlayer(musicFile);
+		mediaplayer.stop();
 	}
 	
 	// The "BACK" button, directing to the main menu "HALFLIFE"
